69551f882ae6593c5ca118e678599d688d92be95
Leonardo Pinheiro <leoprocha100@gmail.com>
2023-09-21 09:59:26 -0300

    feat(auth): create auth module (#3)

---
+++ src/auth/auth.test.ts
@@ -0,0 +1,58 @@
+import { server, request } from 'tests'
+
+import { Api } from 'src/api'
+
+import { Auth } from './auth'
+
+const api = new Api({ apiKey: 'API_KEY', clientId: 'CLIENT_ID' })
+
+describe('Auth', () => {
+  test('should succeed in creating a public key', async () => {
+    const endpointResponse = {
+      publicKey: '30173513-f2a3-42ba-822e-14078ad74c06',
+      createdAt: '2023-09-20T18:30:22.344Z',
+      clientId: 'CLIENT_ID',
+      expires: 600,
+      scope: ['tokens', 'cards'],
+    }
+
+    server.use(
+      request.post('https://api.malga.io/v1/auth', (_, res, ctx) => {
+        return res(ctx.status(200), ctx.json(endpointResponse))
+      }),
+    )
+
+    const auth = new Auth(api)
+    const response = await auth.createPublicKey({
+      expires: 600,
+      scope: ['tokens', 'cards'],
+    })
+
+    expect(response).toMatchObject(endpointResponse)
+  })
+
+  test('should result in an error when creating a public key', async () => {
+    server.use(
+      request.post('https://api.malga.io/v1/auth', (_, res, ctx) => {
+        return res(ctx.status(500))
+      }),
+    )
+
+    const auth = new Auth(api)
+
+    try {
+      await auth.createPublicKey({
+        expires: 600,
+        scope: ['tokens', 'cards'],
+      })
+    } catch (err) {
+      expect(err).toMatchObject({
+        error: {
+          type: 'api_error',
+          code: 500,
+          message: 'unexpected error',
+        },
+      })
+    }
+  })
+})
---
+++ src/auth/auth.ts
@@ -0,0 +1,41 @@
+import { Api, ApiPostOptions } from 'src/api'
+
+import {
+  AuthCreatePublicKeyPayload,
+  AuthCreatePublicKeyResponse,
+} from './auth.types'
+
+export class Auth {
+  constructor(private readonly api: Api) {}
+
+  /**
+   * Create a publicKey
+   *
+   * @link https://docs.malga.io/api#operation/create_auth_token
+   *
+   * @param payload - Send the `scope` that determines access to endpoints and the `expires` in seconds for the key expiration time
+   * @param options - API options like `idempotencyKey`
+   * @returns Customer API response
+   *
+   * @example
+   * ```
+   * import { Malga } from 'malga'
+   *
+   * const malga = new Malga({
+   *   apiKey: 'API_KEY',
+   *   clientId: 'CLIENT_ID',
+   * })
+   *
+   * malga.auth.createPublicKey({
+   *   scope: ['tokens', 'cards'],
+   *   expires: 600, // in seconds
+   * })
+   * ```
+   */
+  public async createPublicKey(
+    payload: AuthCreatePublicKeyPayload,
+    options?: ApiPostOptions,
+  ): Promise<AuthCreatePublicKeyResponse> {
+    return this.api.post('/auth', payload, options?.idempotencyKey)
+  }
+}
